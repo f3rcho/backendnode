4b64b7836fd53eaaf9f0a8fef299374cfaf36fa8
Fernando Cordero <fernan942@gmail.com>
2019-11-16 19:41:27 -0300

    REcibir info desde body y query_8

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const router = express.Router() //nos permite separar cabeceras, metodos, url
 
 var app = express()
-
+app.use(bodyParser.json()) // agregamos json para que acepte las peticiones de ese tipo, asi como urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(router)
 
 router.get('/message', function(req, res) {
-    res.send('Lista de mesajes')
+    console.log(req.body)
+    console.log(req.query)
+    res.send('Lista de mesajes ' + req.body.text + ' añadido correctamente')
 })
 
 router.post('/message', function(req, res) {
@@ -16,10 +20,6 @@ router.post('/message', function(req, res) {
 router.delete('/message', function(req, res) {
     res.send('Mensaje eliminado')
 })
-
-// app.use('/', function(req, res) {
-//     res.send('Hola')
-// })
 
 app.listen(3000)
 console.log('La app esta escuchando en http://localhost:3000')
