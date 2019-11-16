f8b30ca12f6545311b7b705799cb6b4ff35fc01a
Fernando Cordero <fernan942@gmail.com>
2019-11-16 19:10:42 -0300

    Server basico con expresss_7

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 const express = require('express')
+const router = express.Router() //nos permite separar cabeceras, metodos, url
 
 var app = express()
+
+app.use(router)
+
+router.get('/message', function(req, res) {
+    res.send('Lista de mesajes')
+})
+
+router.post('/message', function(req, res) {
+    res.send('Mensaje añadido')
+})
+
+router.delete('/message', function(req, res) {
+    res.send('Mensaje eliminado')
+})
+
+// app.use('/', function(req, res) {
+//     res.send('Hola')
+// })
+
+app.listen(3000)
+console.log('La app esta escuchando en http://localhost:3000')
