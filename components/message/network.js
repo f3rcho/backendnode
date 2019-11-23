b28419034d0986f6639e154c596624549ce7f07c
Fernando Cordero <fernan942@gmail.com>
2019-11-23 00:56:42 -0300

    MongoDB II: Actualizar datos

--- components/message/network.js
+++ components/message/network.js
@@ -25,6 +25,17 @@ router.post('/', function (req, res) {
         })
 })
 
+router.patch('/:id', function (req, res) {
+
+    controller.updateMessage(req.params.id, req.body.message)
+        .then((data) => {
+            response.success(req, res, data, 200)
+        })
+        .catch(e => {
+            response.error(req, res, 'Error interno', 500, e)
+        })
+} )
+
 router.delete('/message', function(req, res) {
     res.status(201).send({error: '', body: 'Eliminado correctamente'}) // podemos cualquier cosa, arrayas, objetos
 })
